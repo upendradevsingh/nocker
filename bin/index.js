100954302ed8fe918e0d4595a3c2694c337db290
Upendra Dev Singh <upendradevsingh@outlook.com>
2018-02-15 02:42:58 +0000

    UPDATE: add support env vars

--- bin/index.js
+++ bin/index.js
@@ -16,10 +16,12 @@ program
 	.option('-p, --port <port>', "port to be opened i.e. 80:80 1337:1337")
 	.option('-n, --name <name>', "container name")
 	.option('-i, --image <image>', 'docker image')
+	.option('-e, --env <env>', 'env variable')
 	.action(function(){
+		var envVar = program.env;
 		var port = program.port +':'+program.port;
 		var vol = program.volume === undefined ? appRoot + ':' +dockerAppRoot : program.volume.indexOf(':') !== -1 ? program.volume : program.volume + ':' + dockerAppRoot;
-		var cmd = 'docker run -d -p 80:80 -p 6379:6379 -p ' + port + ' -v ' + vol + ' --name ' + program.name + ' -i ' +  program.image + ' /bin/bash -c "redis-server --daemonize yes; /root/start_node start; /usr/bin/supervisord"';
+		var cmd = 'docker run -d -p 80:80 -p 6379:6379 -p ' + port + '-e ' + envVar + ' -v ' + vol + ' --name ' + program.name + ' -i ' +  program.image + ' /bin/bash -c "redis-server --daemonize yes; /root/start_node start; /usr/bin/supervisord"';
 		console.log("Running %s", cmd);
 		exec(cmd, function(error, stdout, stderr){
 
